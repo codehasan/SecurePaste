125f9118a3ea93df275b543753628fc2859b90ff
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2024-07-12 11:29:10 +0600

    fix: logo load delay and dropdown menu visibility

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -8,21 +8,31 @@ import { FiMenu, FiPlus } from 'react-icons/fi';
 import classNames from 'classnames';
 import { IoMdClose } from 'react-icons/io';
 import { headers } from 'next/headers';
+import Logo from '@/icons/Logo';
+import TextLogo from '@/icons/TextLogo';
 
 import styles from './NavBar.module.css';
 
 const NavBar = async () => {
-  const nextHeaders = headers();
-  const pathname = nextHeaders.get('x-pathname');
   const supabase = createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
   const pageNavigations = [
-    { name: 'Home', path: '/', active: false },
-    { name: 'Pastes', path: '/pastes', active: false },
-    { name: 'Comments', path: '/comments', active: false },
+    { name: 'Home', path: '/', active: false, requiresUser: false },
+    {
+      name: 'Pastes',
+      path: `/user/${user?.id}/pastes`,
+      active: false,
+      requiresUser: true,
+    },
+    {
+      name: 'Comments',
+      path: `/user/${user?.id}/comments`,
+      active: false,
+      requiresUser: true,
+    },
   ];
 
   const profileNavigations = [
@@ -31,6 +41,9 @@ const NavBar = async () => {
     { name: 'Sign out', path: '/auth/signout' },
   ];
 
+  // Setup active page in navigation menu
+  const nextHeaders = headers();
+  const pathname = nextHeaders.get('x-pathname');
   if (pathname) {
     if (pathname === '/') {
       pageNavigations[0].active = true;
@@ -45,7 +58,7 @@ const NavBar = async () => {
   }
 
   return (
-    <nav className="bg-white shadow-md w-full">
+    <nav className="bg-white shadow-md w-full relative">
       <div className="mx-auto px-2 sm:px-4 lg:px-8">
         <div className="navbar justify-between p-0">
           <div className="flex items-center h-16 px-2 lg:px-0">
@@ -54,26 +67,14 @@ const NavBar = async () => {
               href="/"
               rel="noopener noreferrer"
             >
-              <Image
-                className="max-w-full w-auto h-8"
-                alt="Logo"
-                src="/icon.svg"
-                width={40}
-                height={40}
-              />
-              <Image
-                className="max-w-full w-auto h-8 ml-2 hidden lg:block"
-                alt="Logo Text"
-                src="/logo-text.svg"
-                priority={false}
-                loading="lazy"
-                width={40}
-                height={40}
-              />
+              <Logo className="max-w-full w-auto h-8 text-teal-700" />
+              <TextLogo className="max-w-full w-auto h-7 text-teal-700 ml-2 hidden md:block" />
             </Link>
 
             <div className="hidden lg:ml-6 lg:flex h-full space-x-4">
               {pageNavigations.map((navigation, index) => {
+                if (!user && navigation.requiresUser) return <></>;
+
                 return (
                   <Link
                     key={index}
@@ -92,44 +93,118 @@ const NavBar = async () => {
           </div>
 
           <div className="flex grow shrink basis-0 justify-center px-2 lg:justify-end lg:ml-6">
-            <div className="w-full max-w-lg lg:max-w-80">
-              <label htmlFor="search" className="sr-only">
-                Search
-              </label>
-              <div className="relative">
-                <div className="flex items-center absolute left-0 top-0 bottom-0 pl-3 pointer-events-none">
-                  <IoSearch className="size-5 text-gray-400" />
-                </div>
-                <input
-                  id="search"
-                  name="search"
-                  type="search"
-                  className="text-gray-900 rounded-md w-full pr-3 pl-10 py-1.5 ring-1 ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="Search"
-                  autoComplete="off"
-                />
+            <div className="relative w-full max-w-lg lg:max-w-80">
+              <div className="flex items-center absolute left-0 top-0 bottom-0 pl-3 pointer-events-none">
+                <IoSearch className="size-5 text-gray-400" />
               </div>
+              <input
+                id="search"
+                name="search"
+                type="search"
+                className="text-gray-900 placeholder-gray-400 rounded-md w-full pr-3 pl-10 py-1.5 ring-1 ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Search"
+                autoComplete="off"
+              />
             </div>
           </div>
 
           <div className="flex items-center lg:hidden">
             {user ? (
-              <button
-                type="button"
-                className="text-gray-500 p-2 rounded-md inline-flex justify-center relative cursor-pointer hover:text-gray-600 hover:bg-gray-100"
-              >
-                <span className="absolute -inset-1.5"></span>
-                <span className="sr-only">Open main menu</span>
-                <FiMenu
-                  className={classNames('size-6 text-inherit', styles.menuOpen)}
+              <>
+                <input
+                  type="checkbox"
+                  className={styles.dropdownActive}
+                  id="dropdown-active"
+                  hidden
+                  aria-hidden
                 />
-                <IoMdClose
+
+                <label
+                  htmlFor="dropdown-active"
                   className={classNames(
-                    'size-6 text-inherit',
+                    'text-gray-500 p-2 rounded-md inline-flex justify-center cursor-pointer hover:text-gray-600 hover:bg-gray-100',
+                    styles.menuOpen
+                  )}
+                >
+                  <FiMenu className="size-6 text-inherit" />
+                </label>
+
+                <label
+                  htmlFor="dropdown-active"
+                  className={classNames(
+                    'text-gray-500 p-2 rounded-md inline-flex justify-center cursor-pointer hover:text-gray-600 hover:bg-gray-100',
                     styles.menuClose
                   )}
-                />
-              </button>
+                >
+                  <IoMdClose className="size-6 text-inherit" />
+                </label>
+
+                <div
+                  className={classNames(
+                    'absolute left-0 top-full w-full z-[10] bg-white shadow-md',
+                    styles.menu
+                  )}
+                >
+                  <div className="pt-2 pb-3 space-y-1">
+                    {pageNavigations.map((navigation, index) => {
+                      if (!user && navigation.requiresUser) return <></>;
+
+                      return (
+                        <Link
+                          key={index}
+                          className={
+                            navigation.active
+                              ? 'text-indigo-700 bg-indigo-50 border-l-4 border-indigo-500 font-medium text-base pr-4 pl-3 py-2 block'
+                              : 'text-gray-600 border-l-4 border-transparent font-medium text-base pr-4 pl-3 py-2 block hover:text-gray-700 hover:bg-gray-50 hover:border-gray-400'
+                          }
+                          href={navigation.path}
+                        >
+                          {navigation.name}
+                        </Link>
+                      );
+                    })}
+                  </div>
+                  <div className="pt-4 pb-3 border-gray-200 border-y border-b-0">
+                    <div className="flex items-center px-4">
+                      <Image
+                        className="size-10 max-w-full rounded-full"
+                        alt="Profile"
+                        src={user.user_metadata.avatar || '/img/avatar.svg'}
+                        width={40}
+                        height={40}
+                      />
+                      <div className="ml-3">
+                        <div className="text-gray-800 font-medium text-base">
+                          {user.user_metadata.first_name}&nbsp;
+                          {user.user_metadata.last_name}
+                        </div>
+                        <div className="text-gray-500 font-medium text-sm">
+                          {user.email || 'Email not set'}
+                        </div>
+                      </div>
+                      <Link
+                        href="/notifications"
+                        className="ml-auto text-gray-500 border border-gray-400 border-solid rounded-md p-1 hover:bg-gray-50 hover:text-gray-600 hover:border-gray-500"
+                      >
+                        <RiInbox2Line className="size-5" />
+                      </Link>
+                    </div>
+                    <div className="mt-3">
+                      {profileNavigations.map((navigation, index) => {
+                        return (
+                          <Link
+                            key={index}
+                            className="text-gray-500 font-medium text-base px-4 py-2 block hover:text-gray-600 hover:bg-gray-50"
+                            href={navigation.path}
+                          >
+                            {navigation.name}
+                          </Link>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </>
             ) : (
               <Link href="/auth/signin" className="ml-2">
                 <button className="font-medium text-sm py-1.5 px-2 rounded-md border border-gray-400 border-solid text-gray-500 hover:bg-gray-50">
@@ -154,10 +229,8 @@ const NavBar = async () => {
 
                   <Link
                     href="/notifications"
-                    className="ml-3 text-gray-500 border border-gray-400 border-solid rounded-md relative p-1 hover:bg-gray-50 hover:text-gray-600 hover:border-gray-500"
+                    className="ml-3 text-gray-500 border border-gray-400 border-solid rounded-md p-1 hover:bg-gray-50 hover:text-gray-600 hover:border-gray-500"
                   >
-                    <span className="absolute -inset-1.5"></span>
-                    <span className="sr-only">View notifications</span>
                     <RiInbox2Line className="size-5" />
                   </Link>
 
@@ -165,14 +238,12 @@ const NavBar = async () => {
                     <div
                       tabIndex={0}
                       role="button"
-                      className="rounded-full flex items-center relative cursor-pointer"
+                      className="rounded-full flex items-center cursor-pointer"
                     >
-                      <span className="absolute -inset-1.5"></span>
-                      <span className="sr-only">Open user menu</span>
                       <Image
                         className="size-8 max-w-full rounded-full"
                         alt="Profile"
-                        src="/img/avatar.svg"
+                        src={user.user_metadata.avatar || '/img/avatar.svg'}
                         width={40}
                         height={40}
                       />
@@ -214,69 +285,6 @@ const NavBar = async () => {
           </div>
         </div>
       </div>
-
-      {user && (
-        <div className={classNames('lg:hidden', styles.menu)}>
-          <div className="pt-2 pb-3 space-x-1">
-            {pageNavigations.map((navigation, index) => {
-              return (
-                <Link
-                  key={index}
-                  className={
-                    navigation.active
-                      ? 'text-indigo-700 bg-indigo-50 border-l-4 border-indigo-500 font-medium text-base pr-4 pl-3 py-2 block'
-                      : 'text-gray-600 border-l-4 border-transparent font-medium text-base pr-4 pl-3 py-2 block'
-                  }
-                  href={navigation.path}
-                >
-                  {navigation.name}
-                </Link>
-              );
-            })}
-          </div>
-          <div className="pt-4 pb-3 border-gray-200 border-y border-b-0">
-            <div className="flex items-center px-4">
-              <Image
-                className="size-10 max-w-full rounded-full"
-                alt="Profile"
-                src="/img/avatar.svg"
-                width={40}
-                height={40}
-              />
-              <div className="ml-3">
-                <div className="text-gray-800 font-medium text-base">
-                  {user.user_metadata.first_name}&nbsp;
-                  {user.user_metadata.last_name}
-                </div>
-                <div className="text-gray-500 font-medium text-sm">
-                  {user.email || 'Email not set'}
-                </div>
-              </div>
-              <Link
-                href="/notifications"
-                className="ml-auto text-gray-500 border border-gray-400 border-solid rounded-md relative p-1 hover:bg-gray-50 hover:text-gray-600 hover:border-gray-500"
-              >
-                <span className="absolute -inset-1.5"></span>
-                <span className="sr-only">View notifications</span>
-                <RiInbox2Line className="size-5" />
-              </Link>
-            </div>
-            <div className="mt-3">
-              {profileNavigations.map((navigation, index) => {
-                return (
-                  <Link
-                    key={index}
-                    className="text-gray-500 font-medium text-base px-4 py-2 block"
-                    href={navigation.path}
-                  >
-                    {navigation.name}
-                  </Link>
-                );
-              })}
-            </div>
-          </div>
-        </div>
-      )}
     </nav>
   );
 };
