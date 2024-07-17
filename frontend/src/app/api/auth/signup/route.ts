5e8dfd734a0e84105480929319d7f3a8b3c2bf52
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2024-07-17 20:12:58 +0600

    feat: add user details in postgresql

--- frontend/src/app/api/auth/signup/route.ts
+++ frontend/src/app/api/auth/signup/route.ts
@@ -1,51 +1,97 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
 import { ZodError } from 'zod';
-import { NewUser, SignUpShcema } from '@/lib/schema/ZodSchema';
+import { NewUser, SignUpSchema } from '@/lib/schema/ZodSchema';
 import getErrorMessage from '@/utils/supabase/errors';
 import logger from '@/lib/logging/server';
+import prisma from '@/utils/prisma/db';
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
-  const validation = SignUpShcema.safeParse(body);
+  try {
+    const body = await request.json();
+    const validation = SignUpSchema.safeParse(body);
 
-  // Valildate the request body
-  if (!validation.success) {
-    const error: ZodError = validation.error;
+    // Valildate the request body
+    if (!validation.success) {
+      const error: ZodError = validation.error;
 
-    logger.warn(`Validation error: ${JSON.stringify(error.issues)}`);
+      logger.warn(`Validation error: ${JSON.stringify(error.issues)}`);
 
-    return NextResponse.json(
-      { error: error.issues[0].message },
-      { status: 400 }
-    );
-  }
+      return NextResponse.json(
+        { error: error.issues[0].message },
+        { status: 400 }
+      );
+    }
 
-  const inputData: NewUser = validation.data;
-  const supabase = createClient();
-  const { error } = await supabase.auth.signUp({
-    email: inputData.email,
-    password: inputData.password,
-    options: {
-      captchaToken: inputData.captchaToken,
-    },
-  });
+    const inputData: NewUser = validation.data;
+    const supabase = createClient();
+    const {
+      error,
+      data: { user },
+    } = await supabase.auth.signUp({
+      email: inputData.email,
+      password: inputData.password,
+      options: {
+        captchaToken: inputData.captchaToken,
+      },
+    });
 
-  if (error) {
-    logger.error(`Auth error: ${JSON.stringify(error)}`);
+    // Check for errors in supabase auth
+    if (error) {
+      logger.error(`Auth error: ${JSON.stringify(error)}`);
 
+      return NextResponse.json(
+        {
+          error: getErrorMessage(error),
+        },
+        { status: 500 }
+      );
+    }
+
+    // There are no errors, now check if the user is valid
+    if (!user) {
+      logger.error('No user returned from Supabase after signup.');
+
+      return NextResponse.json(
+        {
+          error: 'An internal error occurred. Please try again later.',
+        },
+        { status: 500 }
+      );
+    }
+
+    // Supabase returned a user, now save the info in postgresql
+    try {
+      await prisma.user.create({
+        data: {
+          id: user.id,
+          name: inputData.name,
+        },
+      });
+    } catch (e) {
+      logger.error(`Postgresql error: ${JSON.stringify(e)}`);
+
+      return NextResponse.json(
+        {
+          error: 'An internal error occured. Please try again later.',
+        },
+        { status: 500 }
+      );
+    }
+
+    // The account is created
     return NextResponse.json(
       {
-        error: getErrorMessage(error),
+        message: `Account has been created.`,
       },
+      { status: 201 }
+    );
+  } catch (err) {
+    logger.error(`Unexpected error: ${JSON.stringify(err)}`);
+
+    return NextResponse.json(
+      { error: 'An unexpected error occurred.' },
       { status: 500 }
     );
   }
-
-  return NextResponse.json(
-    {
-      message: `Account has been created.`,
-    },
-    { status: 201 }
-  );
 }
