a7a0430c5e27e5d873ee7b646a0ea75ab6db1f7a
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2024-07-26 08:07:35 +0600

    feat: get user metadata with id only

--- frontend/src/utils/supabase/user.ts
+++ frontend/src/utils/supabase/user.ts
@@ -2,26 +2,32 @@ import { SupabaseClient, User } from '@supabase/supabase-js';
 import prisma from '../prisma/db';
 import { User as PrismaUser } from '@prisma/client';
 
-export default async function getUser(supabase: SupabaseClient) {
+export default async function getUser(supabase: SupabaseClient, id?: string) {
+  let authUser: User | null = null;
+  let dbUser: PrismaUser | null = null;
+
   try {
     const {
-      data: { user: authUser },
+      data: { user },
     } = await supabase.auth.getUser();
-    let dbUser: PrismaUser | null = null;
+    authUser = user;
+  } catch (error) {
+    console.error('Supabase user error:', JSON.stringify(error));
+  }
 
-    if (authUser) {
+  try {
+    if (id || authUser) {
       dbUser = await prisma.user.findUnique({
         where: {
-          id: authUser.id,
+          id: id || authUser?.id,
         },
       });
     }
-
-    return { authUser, dbUser };
   } catch (error) {
     console.error('Error fetching user:', JSON.stringify(error));
-    return { authUser: null, dbUser: null };
   }
+
+  return { authUser, dbUser };
 }
 
 export async function getUserInfoById(id: string) {
