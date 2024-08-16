0ab46307040dce71a6d5f6b71f8971bf3b2e9d3d
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2024-08-16 16:37:42 +0600

    fix: console logging only in development mode

--- frontend/src/lib/logging/server.ts
+++ frontend/src/lib/logging/server.ts
@@ -1,4 +1,5 @@
-import { createLogger, format, transports } from 'winston';
+import winston, { createLogger, format, transports } from 'winston';
+import { Console } from 'winston/lib/winston/transports';
 
 const logger = createLogger({
   level: 'info',
@@ -10,10 +11,17 @@ const logger = createLogger({
     )
   ),
   transports: [
-    new transports.Console(),
     new transports.File({ filename: '.errors/error.log', level: 'error' }),
     new transports.File({ filename: '.errors/combined.log' }),
   ],
 });
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new Console({
+      format: winston.format.simple(),
+    })
+  );
+}
 
 export default logger;
