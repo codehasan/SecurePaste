d8da6990e6d1dfbea22d17774fc04b35b11d9406
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2024-08-16 16:38:28 +0600

    feat: add function to construct encoded url with queries

--- frontend/src/lib/RedirectHelper.ts
+++ frontend/src/lib/RedirectHelper.ts
@@ -12,3 +12,15 @@ export const push = (pathname: string, params: Record<string, string> = {}) => {
     searchParams.size > 0 ? `${pathname}?${searchParams.toString()}` : pathname;
   redirect(url, RedirectType.push);
 };
+
+export const constructUrl = (pathname: string, params: Record<string, any>) => {
+  const searchParams = new URLSearchParams();
+
+  for (const [key, value] of Object.entries(params)) {
+    searchParams.append(key, value);
+  }
+
+  return searchParams.size > 0
+    ? `${pathname}?${searchParams.toString()}`
+    : pathname;
+};
