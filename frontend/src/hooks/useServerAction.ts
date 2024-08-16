653c8cbd5df1bbd8d62bdcc8a712a9a69ce3dc14
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2024-08-16 16:39:30 +0600

    feat: add server action hook to invoke server actions

---
+++ frontend/src/hooks/useServerAction.ts
@@ -0,0 +1,36 @@
+import { useState, useTransition } from 'react';
+
+const useServerAction = <T extends (...args: any[]) => Promise<any>>(
+  func: T,
+  throwErrors: boolean = false
+) => {
+  const [value, setValue] = useState<ReturnType<T> | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, startTransition] = useTransition();
+
+  const execute = (...params: Parameters<T>) => {
+    setError(null);
+
+    startTransition(async () => {
+      try {
+        const result = await func(...params);
+        setValue(result);
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e);
+        } else {
+          console.error(`Unexpected server action error: ${JSON.stringify(e)}`);
+          setError(new Error('Oops! An unexpected error occured.'));
+        }
+
+        if (throwErrors) {
+          throw e;
+        }
+      }
+    });
+  };
+
+  return { loading, value, error, execute };
+};
+
+export default useServerAction;
