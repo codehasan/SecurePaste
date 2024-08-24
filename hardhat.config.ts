e1a939fbfb9637dac92b0e8b0f888025e62dbaa8
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2024-08-24 10:33:10 +0600

    feat: use env variables for sepolia network and accounts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox';
+import { config as dotConfig } from 'dotenv';
+import { HardhatUserConfig } from 'hardhat/config';
+
+dotConfig({ path: './frontend/.env.local' });
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: '0.8.20',
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.NEXT_PUBLIC_INFURA_SEPOLIA_URL!,
+      accounts: [process.env.WALLET_PRIVATE_KEY!],
+    },
+  },
+  defaultNetwork: 'hardhat',
 };
 
 export default config;
