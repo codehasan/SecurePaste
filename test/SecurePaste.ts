bc5007404908d7916eeb642d6fe85cee24da6b9c
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2024-08-26 18:38:43 +0600

    feat: write tests for contract

--- test/SecurePaste.ts
+++ test/SecurePaste.ts
@@ -1,6 +1,8 @@
 import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
-import hre from 'hardhat';
+import hre, { ethers } from 'hardhat';
 import { SecurePaste, SecurePaste__factory } from '../typechain-types';
+import { expect } from 'chai';
+import { AddressLike, BigNumberish, BytesLike, Typed } from 'ethers';
 
 describe('SecurePaste', () => {
   let wallet1: HardhatEthersSigner,
@@ -17,7 +19,73 @@ describe('SecurePaste', () => {
     it('Should deploy the contract', async () => {
       const factory: SecurePaste__factory =
         await hre.ethers.getContractFactory('SecurePaste');
-      securePaste = await factory.deploy();
+      securePaste = await factory.connect(wallet1).deploy();
+      await securePaste.waitForDeployment();
+
+      securePaste.addListener(
+        'NewPaste',
+        (id: BytesLike | Typed, sender: string) => {
+          console.log(`New paste created by ${sender}, id: ${id}`);
+        }
+      );
+
+      securePaste.addListener(
+        'PasteDeleted',
+        (id: BytesLike | Typed, sender: string) => {
+          console.log(`Paste deleted by ${sender}, id: ${id}`);
+        }
+      );
+
+      securePaste.addListener(
+        'PasteUpdated',
+        (id: BytesLike | Typed, sender: string) => {
+          console.log(`Paste updated by ${sender}, id: ${id}`);
+        }
+      );
+
+      securePaste.addListener(
+        'NewPasteCooldownUpdated',
+        (oldTime: BigNumberish, newTime: BigNumberish) => {
+          console.log(
+            `New paste cooldown updated from ${oldTime} seconds to ${newTime} seconds.`
+          );
+        }
+      );
+
+      securePaste.addListener(
+        'UserPasteLimitUpdated',
+        (oldLimit: BigNumberish, newLimit: BigNumberish) => {
+          console.log(
+            `Individual user paste limit updated from ${oldLimit} to ${newLimit}.`
+          );
+        }
+      );
+
+      expect(securePaste).to.be.not.undefined.and.to.be.not.null;
+    });
+
+    it('Should return correct owner', async () => {
+      const owner = await securePaste.owner();
+      expect(owner).to.be.equal(wallet1.address);
+    });
+  });
+
+  describe('Create Paste', () => {
+    it('Should create a new paste', async () => {
+      const transaction = await securePaste.createPaste(
+        'How to make a promise run with timeout.',
+        ethers.encodeBytes32String(
+          'QmcQvCyLAncpEzvmYtKfvVPVayGqFvTQrYpUpPAJeoXGke'
+        ),
+        'text',
+        Date.now()
+      );
+
+      await transaction.wait();
+
+      const pastes = await securePaste.getAllPaste();
+
+      expect(pastes.length).to.be.equal(1);
     });
   });
 });
