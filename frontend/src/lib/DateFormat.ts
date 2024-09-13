39a86ea94dc16590e26cf02b502528638664b317
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2024-09-13 15:31:42 +0600

    chore: accept Date object as arguments

--- frontend/src/lib/DateFormat.ts
+++ frontend/src/lib/DateFormat.ts
@@ -2,34 +2,42 @@ const dateFormat = new Intl.DateTimeFormat(undefined, {
   dateStyle: 'medium',
 });
 
-export const getTimePassed = (date: string | undefined) => {
+export const getTimePassed = (date: string | undefined | Date) => {
   if (!date) {
     return 'No data';
+  }
+
+  if (date instanceof Date) {
+    getTimePassedFromDate(date);
   }
   return getTimePassedFromDate(new Date(date));
 };
 
-export const getTimePassedFromDate = (past: Date) => {
+const getTimePassedFromDate = (past: Date) => {
   const now = new Date();
   const diffInMilliseconds = now.getTime() - past.getTime();
   const diffInMinutes = Math.floor(diffInMilliseconds / 60000);
   const diffInHours = Math.floor(diffInMinutes / 60);
   const diffInDays = Math.floor(diffInHours / 24);
+  const diffInMonths = Math.floor(diffInDays / 30);
   const diffInYears = Math.floor(diffInDays / 365);
 
   if (diffInYears > 0) {
-    return `${diffInYears}y`;
+    return `${diffInYears} years ago`;
+  }
+  if (diffInMonths > 0) {
+    return `${diffInMonths} months ago`;
   }
   if (diffInDays > 0) {
-    return `${diffInDays}d`;
+    return `${diffInDays} days ago`;
   }
   if (diffInHours > 0) {
-    return `${diffInHours}h`;
+    return `${diffInHours} hours ago`;
   }
   if (diffInMinutes > 0) {
-    return `${diffInMinutes}m`;
+    return `${diffInMinutes} minutes ago`;
   }
-  return '0m';
+  return '0 minutes ago';
 };
 
 export const getFormattedDate = (date: string | Date | undefined) => {
