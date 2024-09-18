4487b4a152bff98ee132df561b00dd6373a6f383
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2024-09-18 12:26:30 +0600

    refactor: remove dependency of prisma to get user data

--- frontend/src/utils/services/user.ts
+++ frontend/src/utils/services/user.ts
@@ -1,44 +1,75 @@
+import { PromiseReturnType } from '@prisma/client/extension';
 import { SupabaseClient, User } from '@supabase/supabase-js';
-import prisma from '../prisma/db';
-import { User as PrismaUser } from '@prisma/client';
+import { validate } from 'uuid';
+
+export interface UserData {
+  id: string;
+  name: string;
+  bio: string | null;
+  avatar: string | null;
+  verified: boolean;
+  created_at: string;
+  pastes_count: number;
+  comments_count: number;
+}
 
 export default async function getUser(supabase: SupabaseClient, id?: string) {
-  let authUser: User | null = null;
-  let dbUser: PrismaUser | null = null;
+  let authUser: User | null = await getAuthUser(supabase);
+  let dbUser: PromiseReturnType<typeof getUserInfoById> | null = null;
 
-  try {
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    authUser = user;
-  } catch (error) {
-    console.error('Supabase user error:', JSON.stringify(error));
+  // Retry to get authentication info
+  if (!authUser) {
+    authUser = await getAuthUser(supabase);
   }
 
-  try {
-    if (id || authUser) {
-      dbUser = await prisma.user.findUnique({
-        where: {
-          id: id || authUser?.id,
-        },
-      });
-    }
-  } catch (error) {
-    console.error('Error fetching user:', error);
+  if (id) {
+    dbUser = await getUserInfoById(supabase, id);
+  } else if (authUser) {
+    dbUser = await getUserInfoById(supabase, authUser.id);
   }
 
   return { authUser, dbUser };
 }
 
-export async function getUserInfoById(id: string) {
-  try {
-    return await prisma.user.findUnique({
-      where: {
-        id,
-      },
-    });
-  } catch (error) {
-    console.error('Error fetching user by ID:', error);
+export async function getUserInfoById(supabase: SupabaseClient, id: string) {
+  if (!validate(id)) {
     return null;
   }
+
+  try {
+    const { data, error } = await supabase.rpc(
+      'get_user_data',
+      {
+        user_id: id,
+      },
+      { get: true }
+    );
+
+    if (error) {
+      console.error('Error fetching user by ID:', error);
+    } else {
+      return data as UserData;
+    }
+  } catch (error) {
+    console.error('Unexpected error fetching user by ID:', error);
+  }
+  return null;
+}
+
+async function getAuthUser(supabase: SupabaseClient) {
+  try {
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.getUser();
+
+    if (error) {
+      console.error('Supabase user error:', error);
+    }
+
+    return user;
+  } catch (error) {
+    console.error('Unexpected supabase user error:', error);
+  }
+  return null;
 }
