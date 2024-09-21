606665e8d07fc27c9da52001ba0e7a74d15db5c6
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2024-09-21 19:26:06 +0600

    feat: create component to handle profile picture selection

---
+++ frontend/src/app/(client)/user/[id]/UploadProfile.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { isJpeg, isPng, isWebP } from '@/lib/FileHelper';
+import { constructUrl } from '@/lib/RedirectHelper';
+import { setProfilePicture } from '@/utils/imgur/actions';
+import classNames from 'classnames';
+import { useRouter } from 'next/navigation';
+import React, { useRef } from 'react';
+import { FaImage } from 'react-icons/fa';
+
+interface UploadProfileProps {
+  text: string;
+  className?: string;
+}
+
+export default function UploadProfile({ text, className }: UploadProfileProps) {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const router = useRouter();
+
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+
+      try {
+        await setProfilePicture(file);
+      } catch (error) {
+        const path = await constructUrl('/error', {
+          message:
+            error instanceof Error
+              ? error.message
+              : 'An unexpected error occurred!',
+        });
+        router.push(path);
+      }
+    }
+  };
+
+  const openFilePicker = () => {
+    fileInputRef.current?.click();
+  };
+
+  return (
+    <>
+      <button
+        className={classNames(
+          className,
+          'btn btn-custom border-gray-300 bg-gray-200 text-gray-900 hover:!bg-gray-300'
+        )}
+        onClick={openFilePicker}
+      >
+        <FaImage />
+        <span>{text}</span>
+      </button>
+
+      <input
+        ref={fileInputRef}
+        type="file"
+        accept="image/png, image/jpeg, image/webp"
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+      />
+    </>
+  );
+}
