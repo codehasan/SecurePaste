7d599448a9905c556c5536baf2142721f93e3894
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2024-09-22 17:51:40 +0600

    fix: helper functions became server actions unexpectedly

--- frontend/src/lib/RedirectHelper.ts
+++ frontend/src/lib/RedirectHelper.ts
@@ -1,16 +1,7 @@
-'use server';
-
 import { redirect, RedirectType } from 'next/navigation';
 
 export const push = (pathname: string, params: Record<string, string> = {}) => {
-  const searchParams = new URLSearchParams();
-
-  for (const [key, value] of Object.entries(params)) {
-    searchParams.append(key, value);
-  }
-
-  const url =
-    searchParams.size > 0 ? `${pathname}?${searchParams.toString()}` : pathname;
+  const url = constructUrl(pathname, params);
   redirect(url, RedirectType.push);
 };
 
